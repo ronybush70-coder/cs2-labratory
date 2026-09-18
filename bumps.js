52f75c60a9418c2b0d40ab35deeab9cf1e4a5b48
ronybush70-coder <ronybush70@gmail.com>
2026-09-18 15:03:20 -0500

    Hourly 15:01 CT: compact Maps1-2 VIT/FURIA + HLTV live Mirage, bumps refresh

--- bumps.js
+++ bumps.js
@@ -1,1 +1,1 @@
-window.__LINE_BUMPS={}; window.REFRESH="Sep 18, 2026 3:01 PM CT";
+window.__LINE_BUMPS={};window.REFRESH="Sep 18, 2026 3:01 PM CT";
--- data.js
+++ data.js
@@ -1,1 +1,1 @@
-var ALL=[]; var REFRESH="Sep 18, 2026 3:01 PM CT";
+var ALL=[];var REFRESH="Sep 18, 2026 3:01 PM CT";
