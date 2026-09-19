e474318aee129312ef4192d3203157e7fa229985
ronybush70-coder <ronybush70@gmail.com>
2026-09-18 19:15:42 -0500

    Add files via upload

--- bumps.js
+++ bumps.js
@@ -1,2 +1,2 @@
-window.__LINE_BUMPS={};
-window.REFRESH="Sep 18, 2026 6:18 PM CT";
+window.__LINE_BUMPS={"kyxsan|Maps 1-2 Kills|AUR vs VIT":{"from":26.5,"to":23.5,"dir":"CUT"}};
+window.REFRESH="Sep 18, 2026 7:15 PM CT";
